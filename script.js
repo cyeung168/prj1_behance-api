07625336b24242a2907e2a3c5b2034142540e94d
Cynthia Yeung <cyeung91@gmail.com>
2015-02-05 00:42:28 -0800

    Reorganized script to loop functions as needed

--- script.js
+++ script.js
@@ -4,58 +4,56 @@ $(document).ready(function(){
 	var portfolioUrl = "http://www.behance.net/v2/users/" + userId + "/projects?client_id=" + apiKey + "&callback=?";
 	$('.intro-reveal').css("display", "none");
 	
-// WEBSITE INTRO
+// INTRO AREA
 (function(){
 	$("#viewer-name").on("keydown", function(event) {
 		if ($("#viewer-name").val().length >= 2) {
 			console.log("value larger than 2");
 			$(".input-cmd").text("Is that what you answer to? If yes, press enter.");
-		}
-
+		} // close .val() if method
 
 		if (event.keyCode===13) {
 			$(".intro-prompt").fadeOut(100);
 			$(".intro-reveal").fadeIn(1000);
-		}
+		} // close keyCode if method
+	});	
 
+
+	$(".show-portfolio").on("click", function(e) {
+			$(".intro-area").fadeOut(100);
+			console.log("fade out intro area");
+		// PORTFOLIO AREA 
+		$.getJSON(portfolioUrl, function(projects) {
+			//console.log(projects);
+			//console.log(projects.projects[0].covers[230]);
+			var source = $("#portfolio-template").html();
+			var template = Handlebars.compile(source);
+			var html = template({projectData: projects});
+			$(".portfolio-area").html(html);
+
+
+			$('.project-item').on('click', function (e) {
+				//console.log("project-item clicked");
+				var projectId = $(this).data("projectid");
+				var projectsUrl = "http://www.behance.net/v2/projects/"+ projectId + "?api_key=" + apiKey + "&callback=?";
+
+				// PROJECT AREA
+				$.getJSON(projectsUrl, function(modules) {
+					console.log(modules);
+					var source = $("#project-template").html();
+					var template = Handlebars.compile(source);
+					var html = template(modules);
+
+					$(".modal-content").html(html);
+					$(".item:first").addClass("active");
+				});	
+
+			});
+
+		});
 	});
 
-	
-})();
 
-// // PORTFOLIO AREA 
-// $.getJSON(portfolioUrl, function(projects) {
-// 	//console.log(projects);
-// 	//console.log(projects.projects[0].covers[230]);
-// 	var source = $("#portfolio-template").html();
-// 	var template = Handlebars.compile(source);
-// 	var html = template({projectData: projects});
+})(); // close self-invoking function
 
-
-// 	$(".portfolio-area").html(html);
-
-
-// 	$('.project-item').on('click', function (e) {
-// 		//console.log("project-item clicked");
-// 		var projectId = $(this).data("projectid");
-// 		var projectsUrl = "http://www.behance.net/v2/projects/"+ projectId + "?api_key=" + apiKey + "&callback=?";
-
-// 		// PROJECT AREA
-// 		$.getJSON(projectsUrl, function(modules) {
-// 			console.log(modules);
-// 			var source = $("#project-template").html();
-// 			var template = Handlebars.compile(source);
-// 			var html = template(modules);
-
-// 			$(".modal-content").html(html);
-// 			$(".item:first").addClass("active");
-
-// 		});	
-
-// 	});
-
-// });
-
-
-
-});
+}); // close document ready
